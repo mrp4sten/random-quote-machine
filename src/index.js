dc32848805e40989a8ef0a41c5172a377da64c95
mrp4sten <mauricio.pasten.martinez@gmail.com>
2024-02-22 21:49:47 -0600

    Add quote and author display functionality
    Resolves: #6 & #7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import axios from 'axios'
-console.log(axios.isCancel('something'))
-console.log('This is loading')
+
+const txtField = document.getElementById('text') // HTML div element called "text"
+const txtAuthor = document.getElementById('author') // HTML div element called "author"
+
+/**
+ * A promise that resolves to an object with a quote and an author
+ * @returns {Promise<Object>}
+ */
+const getQuote = async () => {
+  const category = 'computers'
+  const url = `${process.env.API_URL}?category=${category}`
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'X-Api-Key': process.env.API_KEY,
+        'Content-Type': 'application/json'
+      }
+    })
+    return response.data[0]
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+getQuote().then(quote => {
+  txtField.innerText = quote.quote // User Story #6
+  txtAuthor.innerText = quote.author // User Story #7
+})
